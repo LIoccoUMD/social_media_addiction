e42b107c4af28a9fc1d39ede9e9c285175213c2f
Luciano Iocco <liocco@terpmail.umd.edu>
2025-06-12 12:28:26 -0400

    Orthographic visual prepared

--- vis/visualizations.py
+++ vis/visualizations.py
@@ -1,5 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
 
-fig, ax = plt.subplots(figsize=(10,6))
-plt.show()
+def orthographic():
+    fig = go.Figure(go.Scattergeo())
+    fig.update_geos(projection_type="orthographic")
+    fig.update_geos(showcountries=True)
+    fig.update_geos(showocean=True)
+    fig.update_layout(height=300, margin={"r":0,"t":0,"l":0,"b":0})
+    fig.show()
+    
+orthographic()
